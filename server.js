47a03664716a7183c2ed317ea15e92dffc7fa8d8
olivervolkov <oliver.volkov@tptlive.ee>
2024-01-17 15:44:40 +0200

    nunjucks

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const nunjucks = require('nunjucks')
 const app = express();
 const port = 3000;
+nunjucks.configure('views', {
+    autoescape: true,
+express: app, 
+}),
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
